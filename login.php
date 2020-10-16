5165b72635112f6fe03cf82fe680c5bc04643350
gongarvi <unaimarg@gmail.com>
2020-10-16 08:36:36 +0200

    login y parte de index

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/comun.css">
+    <link rel="stylesheet" href="css/login.css">
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     
@@ -31,7 +32,38 @@
        
     </div>
     <div id="contenido">
-      
+        <div class="formularios">
+            <form name="login" class="from-group">
+                <div>
+                    <label for="email">Email</label>
+                    <input class="form-control" type="text" name="email" id="login_email">
+                </div>
+                <div>
+                    <label for="email">Contraseña</label>
+                    <input class="form-control" type="password" name="password" id="login_password">
+                </div>
+                <input class="btn btn-primary mb-2" type="submit">
+            </form>
+            <form name="register" class="form-group" method="post">
+            <div>
+                <label for="name">Name</label>
+                <input class="form-control" type="text" name="name" id="register_name">
+            </div>        
+                <div>
+                    <label for="surname">Surname</label>
+                    <input class="form-control" type="text" name="surname" id="register_surname">    
+                </div>
+                <div>
+                    <label for="email">Email</label>
+                    <input class="form-control" type="text" name="email" id="register_email">
+                </div>
+                <div>
+                    <label for="email">Contraseña</label>
+                    <input class="form-control" type="password" name="password" id="register_password">
+                </div>
+                <input class="btn btn-primary mb-2" type="submit">
+            </form>
+        </div>
     </div>
     <div id="pie">
       <h1>Lorem Ipsum</h1>
