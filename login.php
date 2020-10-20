7e49c0c3ce8ee601844e6921c8d42de0dc1c35fc
ik_2dw3 <ik_2dw3@H015110.NET>
2020-10-20 08:50:54 +0200

    Validacion2

--- login.php
+++ login.php
@@ -115,7 +115,7 @@
       <h1>Lorem Ipsum</h1>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="javascript/login-register.js   "></script>
+    <script src="javascript/login-register.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 </html>
