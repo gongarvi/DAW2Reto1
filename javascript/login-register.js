263ba503b31c0fd0084375ed571582d60d4a24ac
gongarvi <unaimarg@gmail.com>
2020-10-23 13:20:16 +0200

    login arreglado definitivo

--- javascript/login-register.js
+++ javascript/login-register.js
@@ -1,9 +1,9 @@
-document.getElementById("register_email").addEventListener("change", () => {
+document.getElementById("register_email").addEventListener("keyup", () => {
     var email = document.getElementById("register_email").value;
     if (email == null || email.length == 0 || /^\s+$/.test(email)) {
         document.getElementById("register_email").style.backgroundColor = "white";
     } else {
-        if (comprobarPassword(password)) {
+        if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/).test(email)) {
             document.getElementById("register_email").style.backgroundColor = "red";
         } else {
             document.getElementById("register_email").style.backgroundColor = "white";
@@ -11,13 +11,13 @@ document.getElementById("register_email").addEventListener("change", () => {
     }
 
 });
-document.getElementById("register_password").addEventListener("change", () => {
+document.getElementById("register_password").addEventListener("keyup", () => {
     var password = document.getElementById("register_password").value;
     if (password == null || password.length == 0 || /^\s+$/.test(password)) {
         document.getElementById("register_password").style.backgroundColor = 'white';
     } else {
         //La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.NO puede tener otros símbolos.
-        if (comprobarPassword(password)) {
+        if (!comprobarPassword(password)) {
             document.getElementById("register_password").style.backgroundColor = 'red';
         } else {
             document.getElementById("register_password").style.backgroundColor = "white";
@@ -29,7 +29,6 @@ document.getElementById("register_surname").addEventListener("change", testSomet
 
 document.getElementsByTagName("form")[1].addEventListener("submit",(event)=>{
     let password=document.getElementById("register_password").value;
-    console.log(comprobarPassword(password));
     if(!comprobarPassword(password)){
         event.preventDefault();
     }
@@ -37,7 +36,7 @@ document.getElementsByTagName("form")[1].addEventListener("submit",(event)=>{
 
 
 function comprobarPassword(password){
-    return  (/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/).test(password);
+    return  (/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{6,16}$/).test(password);
 }
 
 function testSomethingWritten(evento) {
@@ -51,7 +50,6 @@ function testSomethingWritten(evento) {
         }
         else{
             evento.target.style.backgroundColor="white";
-            console.log("Funciona");
         }
         
     }
