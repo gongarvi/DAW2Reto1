f0d3acd99e151dd80edab4261318321197093c28
gongarvi <unaimarg@gmail.com>
2020-10-23 10:06:43 +0200

    animacion submenu

--- javascript/submenu-activation.js
+++ javascript/submenu-activation.js
@@ -1,6 +1,9 @@
+//Se limpia el localsotarge cada vez que se cambia de pagina, de esta manera si lelva un tiempo sin entrar a la página, recibirá la última actualziación de los apartados
+localStorage.clear();
+
 var buttons=document.getElementById("menu").getElementsByTagName("button");
 
-
+//Añadiendo eventos en el menu, para poder abrir el submenu adecuado
 for(let i = 0; i < buttons.length; i++){
     var button = buttons.item(i);
     buttons.item(i).addEventListener("click",abrirSubmenu);
@@ -12,11 +15,13 @@ submenu.addEventListener("click",deappendSubmenu);
 function abrirSubmenu(event){
     var tema=event.currentTarget.textContent+"";
     tema=tema.trim();
+    //Se verifica si no se ha cambiado el tema, para evitar hacer  otra petición.
     if(!(localStorage.getItem("tema_seleccionado")+""===tema)){
         localStorage.setItem("tema_seleccionado",tema);
+        //Obtenemos del php los apartados del tema.
         phpCallback(tema)
         .then((resolve)=>{
-            console.log(resolve);
+            //Guardamos en el localstorage el último apartado guardado.
             localStorage.setItem("apartados",JSON.stringify(resolve.apartados));
             localStorage.setItem("id_tema_seleccionado",JSON.stringify(resolve.tema));
             appendToSubmenu(resolve.tema, resolve.apartados);
@@ -33,7 +38,7 @@ function abrirSubmenu(event){
 }
 
 
-
+//Petición ajax para añadir al submenu
 async function phpCallback(tema){
     return new Promise((resolve,reject)=>{
         $.ajax({
@@ -49,10 +54,13 @@ async function phpCallback(tema){
         })
     });
 }
+
+//Función para añadir elementos al submenu, tambien se le da unae clase para los efectos.
 function appendToSubmenu(tema,array){
-    submenu.className="submenu submenu-active";
     var ul=document.createElement("ul");
+    submenu.className="submenu active"
     submenu.appendChild(ul);
+    //submenu.animate([{"z-index": '100'},{"background-color":"rgba(0,0,0,0.5)"}],{duration:2000});
     array.forEach(element => {
         let li = document.createElement("li");
         let a = document.createElement("a");
@@ -61,10 +69,12 @@ function appendToSubmenu(tema,array){
         a.href="contenido.php?tema="+tema+"&apartado="+element.apartado;
         ul.appendChild(li);
     }); 
+    //Esto es para alinear al centro el submenu
     ul.style.marginTop = (-1)*(ul.clientHeight/2)+"px";
 }
 
+//Se elmina la clase sobrate y se elimina del DOM los apartados.
 function deappendSubmenu(){
-    submenu.className="submenu";
+    submenu.className="submenu deactive";
     submenu.innerHTML="";
 }
