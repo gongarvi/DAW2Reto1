31912daf025f5af171899dba754b83aee8250b17
EricEguski <ericeguskizaga@gmail.com>
2020-10-23 12:51:57 +0200

    cargar contenido

--- contenido.php
+++ contenido.php
@@ -7,6 +7,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="css/comun.css">
+    <link rel="stylesheet" href="css/contenido.css">
 </head>
 <body>  
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -16,22 +17,40 @@
             <h1>Blog DAW2</h1>
         </div>
     </div>
-    <div id="menu">
-
-    </div>
-    <div id="submenu">
-
-    </div>
     <?php
         include "menu.php";
     ?>
     <div id="contenido">
         <h1 class="tituloTutorial">Como meter una web en un servidor Ubuntu</h1>
+        
         <div class="divContenido">
             <h1 style="margin-left: 3%; margin-bottom: 3%;" class="pasoTutorial">Paso 1</h1>
-            <img class="imgTutorial" src="https://upload.wikimedia.org/wikipedia/en/9/95/Test_image.jpg" alt="">
-            <p class="textoTutorial">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, quam. Nostrum, numquam! Aliquam veritatis praesentium totam? Magni rerum amet quis, autem maiores, consequuntur consectetur minima reprehenderit assumenda commodi voluptates esse?</p>
-      </div>
+        <!-- Imagen del contenido -->
+        <?php
+            include "sql-connect.php";
+            if (isset($_GET["apartado"])) {
+                $numApartado = $_GET["apartado"];
+            }
+            $query="SELECT contenido.ruta_imagen FROM contenido WHERE contenido.id_apartado=?";
+            $query=$conn->prepare($query);
+            $query->execute([$numApartado]);
+            $ruta=$query->fetch();
+            echo '<img class="imgTutorial" src="'.$ruta['ruta'].'" alt="">';   
+        ?>
+
+        <!-- Texto del contenido -->
+        <?php
+            include "sql-connect.php";
+            if (isset($_GET["apartado"])) {
+                $numApartado = $_GET["apartado"];
+            }
+            $query="SELECT contenido.texto FROM contenido WHERE contenido.id_apartado=?";
+            $query=$conn->prepare($query);
+            $query->execute([$numApartado]);
+            $texto=$query->fetch();
+            echo '<p class="textoTutorial">'.$texto['texto'].'</p>';   
+        ?>
+        </div>
     </div>
     <div id="pie">
       <h1>Lorem Ipsum</h1>
