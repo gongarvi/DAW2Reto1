fd1b3171854f1e1f9d72f229c93b2abed24991a3
EricEguski <ericeguskizaga@gmail.com>
2020-10-26 09:12:55 +0100

    registros bbdd

--- blog_wiki.sql
+++ blog_wiki.sql
@@ -43,3 +43,17 @@ CREATE TABLE comentario(
     FOREIGN KEY (padre) REFERENCES comentario(id) ON UPDATE CASCADE
 );
 
+INSERT INTO `apartado` (`id`, `id_tema`, `nombre`, `fecha`) VALUES
+(1, 1, 'Arrays', '2020-10-23 11:20:34'),
+(2, 1, 'Bucles', '2020-10-23 11:20:34'),
+(3, 2, 'Variables', '2020-10-23 11:24:27'),
+(4, 2, 'Funciones', '2020-10-23 11:24:27');
+
+INSERT INTO `contenido` (`id`, `id_apartado`, `texto`, `ruta_imagen`, `titulo`) VALUES
+(1, 1, 'Esto es una prueba de ARRAYS\r\n\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis assumenda quae nemo veritatis laborum veniam cumque a voluptatem, nihil eos! Obcaecati alias voluptatem sit. Vel nisi tempora quos temporibus?', 'https://cdn.programiz.com/sites/tutorial2program/files/c-arrays.jpg', 'Crear un array'),
+(2, 2, 'Texto de  prueba de BUCLES\r\n\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis assumenda quae nemo veritatis laborum veniam cumque a voluptatem, nihil eos! Obcaecati alias voluptatem sit. Vel nisi tempora quos temporibus?', 'https://www.explicacion.net/wp-content/uploads/2019/01/ciclos-de-programacion.jpg', ''),
+(3, 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in', 'https://www.nintenderos.com/wp-content/uploads/2019/11/slack-imgs.com_-7-e1573560248585.jpg', 'Modificar un array');
+
+INSERT INTO `tema` (`id`, `nombre`, `color_asociado`, `color_texto`) VALUES
+(1, 'PHP', 'blue', 'white'),
+(2, 'JavaScript', 'orange', 'black');
--- crear_contenido.php
+++ crear_contenido.php
@@ -7,7 +7,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="css/comun.css">
-    <link rel="stylesheet" href="css/contenido.css">
+    <link rel="stylesheet" href="css/crear_contenido.css">
 </head>
 <body>  
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -21,13 +21,22 @@
         include "menu.php";
     ?>
     <div id="contenido">
-        
-        <?php
-            include "sql-connect.php";
-        ?>
-        
+        <div id="crearNuevo">
+            <h1>Crear contenido</h1>
+            <form id="formularioNuevo" name="login" class="from-group" method="post" action="logeo.php">
+            <div>
+                <label for="email">Email</label>
+                <input class="form-control" type="text" name="email" id="login_email" required>
+            </div>
+            <div>
+                <label for="email">Contraseña</label>
+                <input class="form-control" type="password" name="password" id="login_password" required>
+            </div>
+            <input class="btn btn-primary mb-2" type="submit" value="Login">
+        </form>
+        </div>
     </div>
-    </div>
+
     <div id="pie">
       <h1>Lorem Ipsum</h1>
     </div>
