a169b86afa8c6b95bfbc433891fe4886738e06bd
gongarvi <unaimarg@gmail.com>
2020-10-27 11:54:50 +0100

    comentario arreglado

--- comentarios.php
+++ comentarios.php
@@ -7,16 +7,16 @@
     if (isset($_GET["apartado.id_tema"])) {
         $numTema = $_GET["apartado.id_tema"];
     }
-    $query="SELECT usuario.nombre, usuario.apellido ,comentario.texto, comentario.fecha,comentario.padre FROM usuario INNER JOIN  
-        comentario ON comentario.id_usuario=usuario.id  INNER JOIN apartado ON apartado.id=comentario.id_apartado WHERE apartado.id=? 
-        AND apartado.id_tema=?";
+    $query="SELECT usuario.nombre as nombre_usuario, usuario.apellido as apellido_usuario,comentario.texto as comentario, comentario.fecha as fecha,
+        comentario.padre as padre FROM usuario INNER JOIN comentario ON comentario.id_usuario=usuario.id  
+        INNER JOIN apartado ON apartado.id=comentario.id_apartado WHERE apartado.id=? AND apartado.id_tema=?";
     $query=$conn->prepare($query);
     $query->execute(array($numApartado,$numTema));
     $comentarios=$query->fetchAll();
     if($comentarios!=null || count($comentarios)>0){
         echo '<div class="divContenido">';
         foreach($comentarios as $comentario){
-            echo '<p>'.$comentario['usuario.nombre'] ." ". $comentario['usuario.apellido']." ". $comentario['comentario.texto'].'</p><p id="fecha" style= font-family:"italic";
+            echo '<p>'.$comentario['nombre_usuario'] ." ". $comentario['apellido_usuario']." ". $comentario['comentario'].'</p><p id="fecha" style= font-family:"italic";
             font-size:"6px";>'.$comentario['fecha'].'</p>';
         }
         echo '</div>';
