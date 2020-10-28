468182d0a7cbb19d6752b60bd8fd24f42bc45db4
ik_2dw3 <ik_2dw3@H015110.NET>
2020-10-28 14:08:32 +0100

    AñadirComentario

--- coment.php
+++ coment.php
@@ -1,24 +1,40 @@
 <?php
     session_start();
-    $comentario=['comenta'];
-    $tema=$_SESSION['apartado.id_tema'];
-    $apartado=$_SERVER['apartado.id'];
-    $nombre=$_SESSION['name'];
-    $idusuario="SELECT usuario.id FROM usuario INNER JOIN comentario ON comentario.id_usuario=usuario.id WHERE usuario.nombre=$nombre";
-   
-    if(isset($comentario) && isset($idusuario) && isset($apartado)&& $comentario!="" && $idusuario!="" && $apartado!=""){
-        include "sql-connect.php";
-        $query="INSERT INTO comentario(`id_usuario','id_apartado,'texto'`) VALUES (?,?,?)";
-        $query=$conn->prepare($query);
-        $query->execute(
-            array($idusuario,$comentario,$apartado)
-        );  
+    
+    include "sql-connect.php";
+        $comentario=$_POST['comenta'];
+        $tema=$_POST['tema'];
+        $apartado=$_POST['apartado'];
+        $nombre=$_SESSION['email'];
+       echo $nombre;
+    if(isset($comentario) && isset($nombre) && isset($apartado)&& $comentario!="" && $apartado!=""){
+        $query3="SELECT id FROM usuario where correo=?";
+        $query3=$conn->prepare($query3);
+        $query3->execute([$nombre]);
+        $idusuario=$query3->fetch();
+       echo $idusuario['id'];
+        if(isset($idusuario)&& $idusuario!=""){
+            $query="INSERT INTO comentario(`id_usuario`,`id_apartado`,`texto`) VALUES (?,?,?)";
+            $query=$conn->prepare($query);
+            $query->execute(
+                array($idusuario['id'],$apartado,$comentario)
+            );  
+           
+       }
+        else{
+           error();
+           return false;
+         }
+        
     }
     else{
         error();
+        return false;
     }
     function error(){
         setcookie("error","Comentario no valido");
+        
     }
+    
 
 ?>
--- crear_comentarios.php
+++ crear_comentarios.php
@@ -4,6 +4,8 @@
     <form name="coment" class="from-group" method="post" action="coment.php">
         <label for="comentario">Escribe comentario</label>
         <input class="form-control" type="text" name="comenta" id="comenta" required>
+        <input type="hidden" name="apartado" value=<?php echo '"'.$_GET['apartado'].'"';?>>
+        <input type="hidden" name="tema" value=<?php echo '"'.$_GET['tema'].'"';?>>
         <input class="btn btn-primary mb-2" type="submit" value="Añadir">
     </form>
 </div>
