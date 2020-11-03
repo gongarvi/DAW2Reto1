9859b3a6e57ab4249e5da9a88273d6d17048461b
EricEguski <ericeguskizaga@gmail.com>
2020-11-03 08:53:29 +0100

    creacion paginas apartados

--- admin/apartado.php
+++ admin/apartado.php
@@ -1,12 +1,12 @@
 <?php
-    include "rule.php";
+    include "rule.php"
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="./../css/comun.css">
@@ -15,23 +15,37 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <div id="cabecera">
-        <h1><a href="./..">Blog Servidor</a></h1>
+		<h1><a href="./..">Blog Servidor</a></h1>
     </div>
     <?php
-        include "./../menu/menu.php";
+		include "./../menu/menu.php";
         include "../generic-alert/alert.php";
     ?>
     <div id="contenido">
-        <!-- Aqui apareceran las distintas opciones para crear  -->
-        <ul>
-            <li><a href="contenido.php">Administrar Contenido</a></li>
-            <li><a href="apartado.php">Administrar Apartados</a></li>
-            <li><a href="tema.php">Administrar Tema</a></li>
-            <li><a href="usuarios.php">Administrar Usuarios</a></li>
-        </ul>
+		<a href="index.php">Atras</a>
+		<div id="opciones">
+			<a href="apartado.php?option=crear">Crear</a>
+			<a href="apartado.php?option=modificar">Modificar</a>
+			<a href="apartado.php?option=eliminar">Eliminar</a>
+		</div>
+      <?php
+	  	if(isset($_GET["option"])){
+			switch($_GET["option"]){
+				case "crear":
+					include "crear_apartado.php";
+					break;
+				case "modificar":
+						include "modificar_apartado.php";
+						break;
+				case "eliminar":
+					include "eliminar_apartado.php";
+					break;
+			}
+		}
+      ?>
     </div>
     <div id="pie">
-      <h1>Lorem Ipsum</h1>
+      	<h1>Lorem Ipsum</h1>
     </div>
 </body>
 </html>
