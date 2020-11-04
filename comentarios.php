d8a03780fe668c82e1f36f6d4b81572fefb97b38
gongarvi <unaimarg@gmail.com>
2020-11-04 13:32:51 +0100

    ver mas comentarios

--- comentarios.php
+++ comentarios.php
@@ -12,19 +12,19 @@
 
     $query="SELECT usuario.correo as email, usuario.nombre as nombre_usuario, usuario.apellido as apellido_usuario,comentario.texto as comentario, comentario.fecha as fecha,
         comentario.padre as padre, comentario.id as idcomentario FROM usuario INNER JOIN comentario ON comentario.id_usuario=usuario.id  
-        INNER JOIN apartado ON apartado.id=comentario.id_apartado WHERE apartado.id=? AND apartado.id_tema=?";
+        INNER JOIN apartado ON apartado.id=comentario.id_apartado WHERE apartado.id=? AND apartado.id_tema=? LIMIT 10 OFFSET 0";
     $query=$conn->prepare($query);
     $query->execute(array($numApartado,$numTema));
     $comentarios=$query->fetchAll();
     if($comentarios!=null || count($comentarios)>0){
-        echo '<div class="divContenido">';
-        $i=1;
+        echo '<div class="divContenido"> 
+        <div id="comentarios_contenido">';        
         foreach($comentarios as $comentario){
-            echo '<div id="divcoment'.$i.'">
-                <p>'.$comentario['nombre_usuario'] ." ". $comentario['apellido_usuario']." ". 
-                    '<span id="comentario'.$i.'"  contentEditable = "false">'.$comentario["comentario"].'</span>'.
+            echo '<div>
+                <p class="comentario">'.$comentario['nombre_usuario'] ." ". $comentario['apellido_usuario']." ". 
+                    '<span contentEditable = "false">'.$comentario["comentario"].'</span>'.
                 '</p>
-                <p>'.$comentario['fecha'].'</p>'.
+                <p class="fecha">'.$comentario['fecha'].'</p>'.
                 '<form action="editar.php" method="post" >';
                 if($_SESSION["email"]===$comentario["email"]){
                     echo '<input type="button" class="btn btn-info btn_editar" name="editar" value="Editar"/>';
@@ -32,14 +32,16 @@
                 echo '<input type="hidden" name="apartado" value="'.$numApartado.'"> 
                     <input type="hidden" name="tema" value="'.$numTema.'">
                     <input type="hidden" name="id" value="'.$comentario['idcomentario'].'">
-                    <input type="submit" class="btn btn-info" id="guardar'.$i++.'"name="guardar" value="Guardar" hidden/>
+                    <input type="submit" class="btn btn-info" id="guardar" name="guardar" value="Guardar" hidden/>
+                    <input type="hidden" name="comentario" value="'.$comentario["comentario"].'">
                     </form>
             </div>';
                
             
         }
-        echo '</div>';
-        
+        echo '</div>
+        <button class="btn btn_primary btn_verMas">Ver mas</button>
+        </div>';
     }
 ?> 
 <script src="javascript/comentarios.js"></script>
--- javascript/comentarios.js
+++ javascript/comentarios.js
@@ -1,7 +1,63 @@
-$(()=>{
-    $(".btn_editar").click(editarComentario);
-});
 
+$(()=>{
+    var rowCount=$("#comentarios_contenido")[0].childNodes.length;
+    console.log(rowCount);
+    rowCount++;
+    if(rowCount<10){
+        $(".btn_verMas").hide();
+    }else{
+        $(".btn_verMas").click(verMasComentarios);
+    }
+    $(".btn_editar").click(editarComentario);
+    $("form").submit(cambiarComentario);
+    function verMasComentarios(){
+        let params = new URLSearchParams(location.search);
+        let tema = params.get("tema");
+        let apartado = params.get("apartado");
+        peticionAjax("./obtener_todos_comentarios.php","get",{tema:tema,apartado:apartado,rowCount:rowCount})
+        .then((response)=>{
+            if(response!=""){
+                var data = JSON.parse(response);
+                if(data.length!=10){
+                    $(".btn_verMas").hide();
+                }
+                rowCount+=data.length; 
+                for(let i = 0 ; i < data.length ; i++){
+                    cargarDatos(data[i]);
+                }   
+            }else{
+                console.log("no se han podido cargar los comentarios");
+            }
+        })
+        .catch((error)=>{
+            console.error(error);
+        });
+    }
+
+});
+function cambiarComentario(event){
+    //El bloque donde se encuentran los datos del comentario
+    var bloque=event.target.parentNode;
+    console.log(bloque);
+    var comentario_bloque=$((bloque.parentNode)).find("p.comentario span");
+    $(bloque).find("input[name=comentario]").val(comentario_bloque.text());
+}
+
+function cargarDatos(data){
+    let divComentario=$("#comentarios_contenido div")[0];
+    let nuevoDivComentario=divComentario.cloneNode(true);
+    $(nuevoDivComentario).find("p.fecha").text(data.fecha);
+    $(nuevoDivComentario).find("p.comentario").text(data.nombre+" "+data.apellido+" ");
+    let span=document.createElement("span");
+    span.textContent=data.comentario;
+    $(nuevoDivComentario).find("p.comentario").append(span);
+    $(nuevoDivComentario).find("form input[name=id]").val(data.id_comentario);
+    $(nuevoDivComentario).find("form input[name=guardar]").click(cambiarComentario)
+    $(nuevoDivComentario).find("form input[name=editar]").click(editarComentario);
+    $(nuevoDivComentario).find("form input[name=comentario]").val(data.comentario);
+    console.log(nuevoDivComentario);
+    $("#comentarios_contenido").append(nuevoDivComentario);
+}
 function editarComentario(event){
     //Obtener el padre del elemento seleccionado
     var padre=event.target.parentNode;
@@ -13,18 +69,20 @@ function editarComentario(event){
     comentario.attr("contentEditable",true);
     comentario.focus();    
 }
-$("form").submit((event)=>{
-
-    //El bloque donde se encuentran los datos del comentario
-    var bloque=event.target.parentNode;
-    var comentario_bloque=$(bloque).find("p span");
-    var input=document.createElement("input");
-    input.type="hidden";
-    input.name="comentario"
-    input.value=comentario_bloque.text();
-    for(let i = 0;i<document.forms.length;i++){
-        if(document.forms[i]==event.target){
-            document.forms[i].append(input);
-        }
+function peticionAjax(url, type, data){
+    if(url!="" && (type=="post" || type=="get" || type=="delete"|| type=="put") && data!=null){
+        return new Promise((resolve, reject)=>{
+            $.ajax({
+                data: data,
+                url:url,
+                type:type,
+                success:function(response){
+                    resolve(response);
+                },
+                error:function(error){
+                    reject(error);
+                }
+            });
+        });
     }
-});
+}
