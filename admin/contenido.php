a8201bef1d5826767feedaee06d2f1887ce84480
EricEguski <ericeguskizaga@gmail.com>
2020-11-12 08:39:37 +0100

    moduls local

--- admin/contenido.php
+++ admin/contenido.php
@@ -8,14 +8,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<link rel="stylesheet" href="./../moduls/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="./../css/comun.css">
     <link rel="stylesheet" href="./../css/barra.css">
 </head>
 <body>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="./../moduls/jquery/jquery.min.js"></script>
+    <script src="./../moduls/bootstrap/js/bootstrap.min.js"></script>
     <div id="cabecera">
 		<h1><a href="./..">BloG DAW 2</a></h1>
     </div>
